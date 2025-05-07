f8aba143d4588e3d191e5d33c9c3530d4f9e04b7
GerrZa <thanapatr.ger@gmail.com>
2025-05-08 00:28:10 +0700

    add little ui

--- src/battle/ui/custom_progress_bar.gd
+++ src/battle/ui/custom_progress_bar.gd
@@ -11,6 +11,8 @@ var mouse_in = false
 @export var show_name = true
 @export var show_as_percentage = false
 
+@export var slow_bar = false
+
 func _ready() -> void:
 	connect("mouse_entered", func(): mouse_in = true)
 	connect("mouse_exited", func(): mouse_in = false)
@@ -26,8 +28,32 @@ func _process(delta: float) -> void:
 		$tooltip.text = "{1}/{2}".format([value_name, value, max_value])
 		
 	
+	
 	$tooltip.visible = mouse_in
 	
 	value = clamp(value, 0, max_value)
 	
 	$bar.scale.x = value/float(max_value)
+
+
+func _physics_process(delta: float) -> void:
+	if slow_bar:
+		$slow_bar.visible = true
+		$slow_bar.scale.x = lerp($slow_bar.scale.x, $bar.scale.x, 0.1)
+	else:
+		$slow_bar.visible = false
+
+func change_value(new_value):
+	value = new_value
+	$bar.scale.x = value / float(max_value)
+
+func adjust_value(diff):
+	value += diff
+	$bar.scale.x = value / float(max_value)
+
+func reset_value(new_value, new_max_value):
+	value = new_value
+	max_value = new_max_value
+	
+	$bar.scale.x = value / float(max_value)
+	$slow_bar.scale.x = $bar.scale.x
